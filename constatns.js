53ebfdd32efdbff254dd81390abd8d8f3cfed42d
Сергей Маркин <markin.dev@gmail.com>
2020-03-30 23:01:10 +0400

    fix constants

--- constatns.js
+++ constatns.js
@@ -5,8 +5,8 @@ export const LIGHT_GREEN = '#81C784';
 export const BLUE = '#42A5F5';
 export const LIGHT_BLUE = '#64B5F6';
 export const GRAY = '#424242';
-export const WORK_TIMER_SECONDS = 5;
-export const RELAX_TIMER_SECONDS = 5;
+export const WORK_TIMER_SECONDS = 1500;
+export const RELAX_TIMER_SECONDS = 300;
 export const TIMER_STATUSES = {
     stopped: 'stopped',
     running: 'runnig',
